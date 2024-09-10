b9ea93be8624f0b3709a80c6640ed83b0a1e58a5
hadiseh-a <hadisea82@gmail.com>
2024-09-10 20:43:39 +0330

    done tutorial

--- express-tutorial/src/index.mjs
+++ express-tutorial/src/index.mjs
@@ -9,6 +9,18 @@ const loggingMiddleware = (request, response, next) => {
   next();
 };
 
+const resolveIndexByUserId = (request, response, next) => {
+  const {
+    params: { id },
+  } = request;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.statusCode(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  request.findUserIndex = findUserIndex;
+  next();
+};
+
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
@@ -24,15 +36,15 @@ const mockUsers = [
 app.get(
   "/",
   (request, response, next) => {
-    console.log("Base URL");
+    console.log("Base URL 1 ");
     next();
   },
   (request, response, next) => {
-    console.log("Base URL");
+    console.log("Base URL 2");
     next();
   },
   (request, response, next) => {
-    console.log("Base URL");
+    console.log("Base URL 3");
     next();
   },
   (request, response) => {
@@ -53,6 +65,11 @@ app.get("/api/users", (request, response) => {
   return response.send(mockUsers);
 });
 
+app.use(loggingMiddleware, (request, response, next) => {
+  console.log("Finished Logging...");
+  next();
+});
+
 app.post("/api/users", (request, response) => {
   console.log(request.body);
   const { body } = request;
@@ -61,14 +78,9 @@ app.post("/api/users", (request, response) => {
   return response.status(201).send(newUser);
 });
 
-app.get("/api/users/:id", (request, response) => {
-  console.log(request.params);
-  const parsedId = parseInt(request.params.id);
-  console.log(parsedId);
-  if (isNaN(parsedId))
-    return response.status(400).send({ msg: "Bad Request, Invalid ID." });
-
-  const findUser = mockUsers.find((user) => user.id === parsedId);
+app.get("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { findUserIndex } = request;
+  const newUser = { id: mockUsers[findUserIndex] };
   if (!findUser) return response.sendStatus(404);
   return response.send(findUser);
 });
@@ -77,46 +89,20 @@ app.get("/api/products", (request, response) => {
   response.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
 });
 
-app.put("/api/users/:id", (request, response) => {
-  const {
-    body,
-    params: { id },
-  } = request;
-
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return response.sendStatus(400);
-
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-
-  if (findUserIndex === -1) return response.statusCode(404);
-
-  mockUsers[findUserIndex] = { id: parsedId, ...body };
+app.put("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUserIndex } = request;
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
   return response.sendStatus(200);
 });
 
-app.patch("/api/users/:id", (request, response) => {
-  const {
-    body,
-    params: { id },
-  } = request;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return response.statusCode(400);
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) return response.sendStatus(404);
+app.patch("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUserIndex } = request;
   mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
   return response.sendStatus(200);
 });
 
-app.delete("/api/users/:id", (request, response) => {
-  const {
-    params: { id },
-  } = request;
-
-  const parsedId = parseInt(id);
-
-  if (isNaN(parsedId)) return response.sendStatus(400);
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) return response.sendStatus(404);
+app.delete("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { findUserIndex } = request;
   mockUsers.splice(findUserIndex, 1);
   return response.sendStatus(200);
 });
